f71f99deb61d27c1377600e82f523fea36eb3613
Tomas Ortiz <TomasOrtiz@protonmail.com>
2019-11-15 15:59:23 -0800

    fixed a syntax error

--- challenges/functions.js
+++ challenges/functions.js
@@ -6,8 +6,8 @@
   * The last parameter accepts a callback
   * The consume function should return the invocation of cb, passing a and b into cb as arguments
 */
-function consume(a, b, callback)
-return cb(a, b, callback)
+function consume(a, b, cb)
+return cb(a, b,)
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
@@ -22,7 +22,7 @@ function multiply(num1, num2) {
 }
 
 function greeting(first-Name, last-Name) {
- return (`Hello ${first-Name} ${last-Name}, nice to meet you!`);
+ return (`Hello ${first-Name}  ${last-Name}, nice to meet you!`);
 }
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
 console.log(consume(2, 2, add)); // 4
