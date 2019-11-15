5a60c61636d12277f190683def44717d9d2c1e4e
Tomas Ortiz <TomasOrtiz@protonmail.com>
2019-11-15 10:40:21 -0800

    finished prototypes and classes

--- challenges/functions.js
+++ challenges/functions.js
@@ -6,19 +6,28 @@
   * The last parameter accepts a callback
   * The consume function should return the invocation of cb, passing a and b into cb as arguments
 */
-
+function consume(a, b, callback)
+return cb(a, b, callback)
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
+function add(num1, num2) {
+  return num1 + num2;
+}
+function multiply(num1, num2) {
+  return num1 * num2;
+}
 
-
+function greeting(first-Name, last-Name) {
+ return (`Hello ${first-Name} ${last-Name}, nice to meet you!`);
+}
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// console.log(consume(2, 2, add)); // 4
-// console.log(consume(10, 16, multiply)); // 160
-// console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
+console.log(consume(2, 2, add)); // 4
+console.log(consume(10, 16, multiply)); // 160
+console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
@@ -27,6 +36,7 @@
 
 // Explanation: 
 
+// Because its nested inside its in the scope of the function. From looking at the curly braces it's protecting the function from roaming wild outside of the intended scope from what i understand. i'm not sure im understanding the question correctly or if it's really that simple, but it seems TOO simple... i'm suspicious and i expect to be wrong
 
 const external = "I'm outside the function";
 
